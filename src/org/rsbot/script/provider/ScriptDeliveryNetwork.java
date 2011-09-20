06adb0fa4ebb9638910f243cb2b2757bce3132e3
Wang Can <paris@powerbot.org>
2011-09-20 11:45:44 +0100

    Helper method to check if SDN is available.

--- src/org/rsbot/script/provider/ScriptDeliveryNetwork.java
+++ src/org/rsbot/script/provider/ScriptDeliveryNetwork.java
@@ -112,4 +112,8 @@ public class ScriptDeliveryNetwork implements ScriptSource, Runnable {
 	public void run() {
 		refresh(true);
 	}
+
+	public boolean isAvailable() {
+		return base != null;
+	}
 }
