5e5cbd27c05ac6784dfac2b877e389d15bc3b8d2
Wang Can <paris@powerbot.org>
2011-09-21 17:45:31 +0100

    Removed unused class.

--- src/org/rsbot/util/ServiceException.java
+++
@@ -1,9 +0,0 @@
-package org.rsbot.util;
-
-public class ServiceException extends Exception {
-	private static final long serialVersionUID = -8271057217379394057L;
-
-	public ServiceException(final String message) {
-		super(message);
-	}
-}
