3dd9f8b26dd83b64123a304a3c19f05930cfe80e
Munch <mScripts@hotmail.com>
2011-09-22 23:48:14 -0300

    Updated code, removed broken and unwanted code(Community vote).

--- src/org/rsbot/script/randoms/CloseAllInterface.java
+++ src/org/rsbot/script/randoms/CloseAllInterface.java
@@ -2,107 +2,72 @@ package org.rsbot.script.randoms;
 
 import org.rsbot.script.Random;
 import org.rsbot.script.ScriptManifest;
-import org.rsbot.script.util.Filter;
-import org.rsbot.script.wrappers.RSComponent;
-import org.rsbot.script.wrappers.RSNPC;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Updated by Iscream Feb 22,10 Updated by Parameter Jan 1, 11
+ * Closes interfaces that scripts may open by mistake.
  */
-@ScriptManifest(authors = {"Nightmares18", "joku.rules", "Taha", "Fred"}, name = "FrogCave", version = 2.3)
-public class FrogCave extends Random {
-	private RSNPC frog;
-	private boolean talkedToHerald, talkedToFrog;
-	private int tries;
+@ScriptManifest(authors = {"Jacmob", "HeyyamaN", "Pervy Shuya"}, name = "InterfaceCloser", version = 1.9)
+public class CloseAllInterface extends Random {
+	private final Map<Integer, Integer> components = new HashMap<Integer, Integer>();
+
+	{
+		components.put(14, 11); // Pin settings
+		components.put(17, 13); // Death items
+		components.put(18, 37); // "Get Ready to Respawn" Confirm button.
+		//components.put(109, 14); // Grand exchange collection
+		components.put(149, 226); //Level 10 membership solicitation
+		components.put(157, 13); // Quick chat help
+		components.put(206, 13); // Price check
+		components.put(266, 1); // Tombstone
+		components.put(266, 11); // Grove
+		components.put(275, 8); // Quest
+		components.put(276, 76); // Soul Wars Rewards
+		components.put(499, 29); // Stats
+		components.put(594, 48); // Report Abuse
+		components.put(667, 75); // Equipment Bonus
+		components.put(732, 208); // Fist of Guthx Reward Shop
+		components.put(742, 18); // Graphic
+		components.put(743, 20); // Audio
+		components.put(755, 44); // World Map
+		components.put(764, 18); // Objectives
+		components.put(767, 10); // Bank of RuneScape - Help
+		components.put(895, 19); // Advisor
+		components.put(917, 73); // Task List
+		components.put(1011, 51); // Pest Control Rewards ( Commendation Rewards )
+		components.put(1083, 181); // Livid Farm Rewards
+		components.put(1107, 174); // Clan Vexillum
+		components.put(1127, 16); //Hybrid Gloves selection ( Fist of Guthix )
+	}
 
 	@Override
 	public boolean activateCondition() {
-		return npcs.getNearest("Frog Herald") != null && objects.getNearest(5917) != null && game.isLoggedIn();
-	}
-
-	private RSNPC findFrog() {
-		return npcs.getNearest(new Filter<RSNPC>() {
-			public boolean accept(final RSNPC npc) {
-				return !npc.isMoving() && npc.getHeight() == -278;
+		if (game.isLoggedIn()) {
+			for (Map.Entry<Integer, Integer> c : components.entrySet()) {
+				if (interfaces.getComponent(c.getKey(), c.getValue()).isValid()) {
+					return true;
+				}
 			}
-		});
-	}
-
-	private boolean canContinue() {
-		return interfaces.canContinue() || interfaces.getComponent(65, 6).isValid();
-	}
-
-	@Override
-	public void onFinish() {
-		talkedToHerald = false;
-		frog = null;
-		tries = 0;
+		}
+		return false;
 	}
 
 	@Override
 	public int loop() {
-		if (!activateCondition()) {
-			return -1;
+		sleep(random(500, 900));
+		for (final Map.Entry<Integer, Integer> c : components.entrySet()) {
+			if (interfaces.getComponent(c.getKey(), c.getValue()).isValid()) {
+				if (interfaces.getComponent(c.getKey(), c.getValue()).doClick()) {
+					sleep(random(900, 1100));
+					if (random(0, 4) == 0) {
+						mouse.moveSlightly();
+					}
+					break;
+				}
+			}
 		}
-		try {
-			if (canContinue()) {
-				if (!talkedToHerald) {
-					final RSComponent heraldTalkComp = interfaces.getComponent(242, 4);
-					talkedToHerald = heraldTalkComp.isValid() && (heraldTalkComp.containsText("crown") || heraldTalkComp.containsText("is still waiting"));
-				}
-				if (!interfaces.clickContinue()) {
-					interfaces.getComponent(65, 6).doClick();
-				}
-				return random(600, 800);
-			}
-			if (getMyPlayer().isMoving() || getMyPlayer().getAnimation() != -1) {
-				return random(600, 800);
-			}
-			if (!talkedToHerald) {
-				final RSNPC herald = npcs.getNearest("Frog Herald");
-				if (calc.distanceTo(herald) < 5) {
-					if (!calc.tileOnScreen(herald.getLocation())) {
-						camera.turnTo(herald);
-					}
-					herald.interact("Talk-to");
-					return random(500, 1000);
-				} else {
-					walking.walkTileMM(herald.getLocation());
-					return random(500, 700);
-				}
-			}
-			if (frog == null) {
-				frog = findFrog();
-				if (frog != null) {
-					log("Princess found! ID: " + frog.getID());
-				}
-			}
-			if (frog != null && frog.getLocation() != null && (!talkedToFrog || !canContinue())) {
-				if (calc.distanceTo(frog) < 5) {
-					if (!calc.tileOnScreen(frog.getLocation())) {
-						camera.turnTo(frog);
-					}
-					if (frog.interact("Talk-to Frog")) {
-						sleep(750, 1250);
-						talkedToFrog = canContinue();
-					}
-					return random(900, 1000);
-				} else {
-					walking.walkTileMM(frog.getLocation());
-					return random(500, 700);
-				}
-			} else {
-				tries++;
-				if (tries > 200) {
-					// log("tries > 200");
-					tries = 0;
-					talkedToHerald = false;
-				}
-				return random(200, 400);
-			}
-		} catch (final Exception e) {
-			e.printStackTrace();
-		}
-		return random(200, 400);
+		return -1;
 	}
 }
