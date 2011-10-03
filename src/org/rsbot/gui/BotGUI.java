bed34aa2adc1eb1250035274b059cf22fbcef487
Joe <Timer150@Gmail.com>
2011-10-02 21:11:31 -0400

    Deprecated BotGUI.openURL.

--- src/org/rsbot/gui/BotGUI.java
+++ src/org/rsbot/gui/BotGUI.java
@@ -1,6 +1,14 @@
 package org.rsbot.gui;
 
 public final class BotGUI {
+	/**
+	 * Opens an url.
+	 *
+	 * @param url The URL to open.
+	 * @see org.rsbot.gui.Chrome#openURL(String)
+	 * @deprecated Class name changed.
+	 */
+	@Deprecated
 	public static void openURL(final String url) {
 		Chrome.openURL(url);
 	}
