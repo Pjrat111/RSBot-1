4743d34202dbda28ccfbf41205e81894c42e8a83
vJoseph <jovas11789@yahoo.com>
2011-10-06 10:58:10 -0300

    Edited src/org/rsbot/script/methods/Interfaces.java via GitHub

--- src/org/rsbot/script/methods/Interfaces.java
+++ src/org/rsbot/script/methods/Interfaces.java
@@ -129,13 +129,11 @@ public class Interfaces extends MethodProvider {
 		}
 		final RSInterface[] valid = getAll();
 		for (final RSInterface iface : valid) {
-			if (iface.getIndex() == 137)
+			if (iface.getIndex() == 137 || iface.getIndex() == 752)
 				continue;
-			final int len = iface.getChildCount();
-			for (int i = 0; i < len; i++) {
-				final RSComponent child = iface.getComponent(i);
-				if (child.containsText("Click here to continue") && child.isValid() && child.getAbsoluteX() > 10 && child.getAbsoluteY() > 300) {
-					return child;
+			for (final RSComponent c : iface.getComponents()) {
+				if (c.containsText("Click here to continue") && c.isValid() && c.getAbsoluteX() > 10 && c.getAbsoluteY() > 300) {
+					return c;
 				}
 			}
 		}
