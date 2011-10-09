dfb724f17116eec8f596bdc78ac5f14c39a3a804
Wang Can <paris@powerbot.org>
2011-10-09 12:06:23 +0100

    Simplified account saving and loading.

--- src/org/rsbot/script/AccountStore.java
+++ src/org/rsbot/script/AccountStore.java
@@ -4,6 +4,7 @@ import org.rsbot.gui.AccountManager;
 import org.rsbot.security.RestrictedSecurityManager;
 import org.rsbot.util.Base64;
 import org.rsbot.util.StringUtil;
+import org.rsbot.util.io.IniParser;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
@@ -11,18 +12,21 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 /**
+ * @author Paris
  * @author Timer
  */
 public class AccountStore {
 	public static class Account {
+		private final static String PASSWORD = "password";
 		private final String username;
-		private String password;
 		private final Map<String, String> attributes = new TreeMap<String, String>();
 
 		public Account(final String username) {
@@ -34,7 +38,7 @@ public class AccountStore {
 		}
 
 		public String getPassword() {
-			return password;
+			return getAttribute(PASSWORD);
 		}
 
 		public String getAttribute(final String key) {
@@ -46,12 +50,12 @@ public class AccountStore {
 		}
 
 		public void setPassword(final String password) {
-			this.password = password;
+			setAttribute(PASSWORD, password);
 		}
 
 		@Override
 		public String toString() {
-			return "Account[" + username + "]";
+			return username;
 		}
 	}
 
@@ -62,7 +66,7 @@ public class AccountStore {
 
 	private final File file;
 	private byte[] digest;
-	private final String[] protectedAttributes = {"pin"};
+	private final String[] protectedAttributes = {Account.PASSWORD, "pin"};
 
 	private final Map<String, Account> accounts = new TreeMap<String, Account>();
 
@@ -95,79 +99,36 @@ public class AccountStore {
 	}
 
 	public void load() throws IOException {
-		if (!file.exists()) {
-			file.createNewFile();
+		final BufferedReader in = new BufferedReader(new FileReader(file));
+		final String header = in.readLine();
+		if (header == null || header.length() == 0 || Integer.parseInt(header) != FORMAT_VERSION) {
+			throw new IOException("Invalid format");
 		}
-		if (!file.canRead() || !file.canWrite()) {
-			file.setReadable(true);
-			file.setWritable(true);
-		}
-		final BufferedReader br = new BufferedReader(new FileReader(file));
-		try {
-			final int v = Integer.parseInt(br.readLine());
-			if (v != FORMAT_VERSION) {
-				throw new IOException("unsupported format version: " + v);
+		final Map<String, Map<String, String>> data = IniParser.deserialise(in);
+		for (final Entry<String, Map<String, String>> entry : data.entrySet()) {
+			final Account account = new Account(entry.getKey());
+			for (final Entry<String, String> a : entry.getValue().entrySet()) {
+				account.setAttribute(a.getKey(), Arrays.asList(protectedAttributes).contains(a.getKey()) ? decrypt(a.getValue()) : a.getValue());
 			}
-		} catch (final NumberFormatException ex) {
-			throw new IOException("bad format");
+			accounts.put(normaliseUsername(account.getUsername()), account);
 		}
-		accounts.clear();
-		Account current = null;
-		for (; ; ) {
-			final String line = br.readLine();
-			if (line == null) {
-				break;
-			}
-			if (line.startsWith("[") && line.endsWith("]")) {
-				if (current != null) {
-					accounts.put(current.username, current);
-				}
-				final String name = AccountStore.fixName(line.trim().substring(1).substring(0, line.length() - 2));
-				current = new Account(name);
-				continue;
-			}
-			if (current != null && line.matches("^\\w+=.+$")) {
-				final String[] split = line.trim().split("=");
-				if (split[0].equals("password")) {
-					current.password = decrypt(split[1]);
-				} else {
-					if (Arrays.asList(protectedAttributes).contains(split[0])) {
-						split[1] = decrypt(split[1]);
-					}
-					current.setAttribute(split[0], split[1]);
-				}
-			}
-		}
-		if (current != null && !current.getUsername().equalsIgnoreCase(FACEBOOK_ACCOUNT.getUsername())) {
-			accounts.put(current.username, current);
-		}
-		br.close();
+		in.close();
 	}
 
 	public void save() throws IOException {
-		final BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-		bw.write(Integer.toString(FORMAT_VERSION));
-		bw.newLine();
-		for (final String name : accounts.keySet()) {
-			bw.append("[").append(AccountStore.fixName(name.trim())).append("]");
-			bw.newLine();
-			final String password = accounts.get(name).password;
-			if (password != null) {
-				bw.append("password=");
-				bw.append(encrypt(password));
+		final BufferedWriter out = new BufferedWriter(new FileWriter(file));
+		out.append(Integer.toString(FORMAT_VERSION));
+		out.newLine();
+		final Map<String, Map<String, String>> data = new TreeMap<String, Map<String, String>>();
+		for (final Entry<String, Account> account : accounts.entrySet()) {
+			final Map<String, String> attributes = new TreeMap<String, String>();
+			for (final Entry<String, String> a : account.getValue().attributes.entrySet()) {
+				attributes.put(a.getKey(), Arrays.asList(protectedAttributes).contains(a.getKey()) ? encrypt(a.getValue()) : a.getValue());
 			}
-			bw.newLine();
-			for (final Map.Entry<String, String> entry : accounts.get(name).attributes.entrySet()) {
-				final String key = entry.getKey();
-				String value = entry.getValue();
-				if (Arrays.asList(protectedAttributes).contains(key)) {
-					value = encrypt(value);
-				}
-				bw.append(key).append("=").append(value);
-				bw.newLine();
-			}
+			data.put(normaliseUsername(account.getKey()), attributes);
 		}
-		bw.close();
+		IniParser.serialise(data, out);
+		out.close();
 	}
 
 	public void setPassword(final String password) {
@@ -175,13 +136,14 @@ public class AccountStore {
 			digest = null;
 			return;
 		}
+		final MessageDigest md;
 		try {
-			final MessageDigest md = MessageDigest.getInstance("SHA-1");
-			md.update(password.getBytes("iso-8859-1"), 0, password.length());
-			digest = md.digest();
-		} catch (final Exception e) {
+			md = MessageDigest.getInstance("SHA-1");
+		} catch (final NoSuchAlgorithmException ignored) {
 			throw new RuntimeException("Unable to digest password!");
 		}
+		md.update(StringUtil.getBytesUtf8(password));
+		digest = md.digest();
 		digest = Arrays.copyOf(digest, 24);
 		for (int i = 0, off = 20; i < 4; ++i) {
 			digest[off++] = digest[i];
@@ -227,22 +189,14 @@ public class AccountStore {
 	}
 
 	/**
-	 * Capitalizes the first character and replaces spaces with underscores.
-	 * Purely aesthetic.
+	 * Capitalises the first character and replaces spaces with underscores.
 	 *
 	 * @param name The name of the account
-	 * @return Fixed name
+	 * @return Normalised username
 	 */
-	private static String fixName(String name) {
-		if (name.contains("@")) {
-			name = name.toLowerCase().trim();
-		} else {
-			if (name.charAt(0) > 91) {
-				name = (char) (name.charAt(0) - 32) + name.substring(1);
-			}
-			name = name.replaceAll("\\s", "_");
-		}
+	private static String normaliseUsername(String name) {
+		name = name.toLowerCase().trim().replaceAll("\\s", "_");
+		name = name.substring(0, 1).toUpperCase() + name.substring(1);
 		return name;
 	}
 }
-
